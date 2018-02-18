ca9dbd8082c06e797577287fb36a657b24aae88f
DLoT <achleitner.thomas@gmail.com>
2018-02-18 04:47:04 +0100

    rewrite

--- test/test.js
+++ test/test.js
@@ -1,42 +1,28 @@
 'use strict';
-const path   = require('path');
-const assert = require('assert');
-const fs     = require('fs');
-const purger = require('../');
-
-console.log(__dirname);
+const path               = require('path'),
+      assert             = require('assert'),
+      fs                 = require('fs'),
+      purge = require('../');
 
 const
   entrypoint        = path.join(__dirname, 'test-module.js'),
-  targetDir         = './test-dist',
-  rootDir           = path.join(__dirname, '..'),
+  rootPath          = path.join(__dirname, '..'),
+  targetPath        = 'test-dist',
   additionalModules = ['ms'],
 
   options           = {
     entrypoint,
-    targetDir,
-    rootDir,
+    targetPath,
+    rootPath,
     additionalModules,
     manualCopies: [],
   };
 
-console.log(options);
-
-process.stderr.on('data', function (err) {
-  if (err) {
-    console.log('DASD', err.toString());
-  }
-});
-
-process.stdout.on('data', function (data) {
-  console.log(data);
-});
-
-purger(options, () => {
+purge(options).then(() => {
   console.log('purged');
-  const modulesContent = fs.readdirSync(`${targetDir}/node_modules`);
-  assert.deepEqual(modulesContent, ['commander', 'ms']);
-
-  process.exit(0);
-});
+//   const modulesContent = fs.readdirSync(`${targetDir}/node_modules`);
+//   assert.deepEqual(modulesContent, ['commander', 'ms']);
+//
+//   process.exit(0);
+ });
 
