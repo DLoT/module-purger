f3574846cda0591ef33fbc8f2aa559a48003b3d8
Frank Liepert <trainmaster@gmx.net>
2021-05-25 07:52:49 +0200

    Add missing files for scoped packages  (#4)
    * Add test for scoped package
    
    * Assert contents of purged packages
    
    * Add missing files for scoped packages
    
    * Replaced deprecated function

--- test/test.js
+++ test/test.js
@@ -23,7 +23,22 @@ purge(options).then(() => {
   const purgedModules  = path.join(rootPath, options.targetPath, 'node_modules'),
         modulesContent = fs.readdirSync(purgedModules);
 
-  assert.deepEqual(modulesContent, ['commander', 'ms']);
+  assert.deepStrictEqual(modulesContent, ['@acme', 'commander', 'ms']);
+
+  const acmePath    = path.join(purgedModules, '@acme', 'example');
+  const acmeContent = fs.readdirSync(acmePath);
+
+  assert.deepStrictEqual(acmeContent, ['LICENSE.md', 'index.js', 'package.json']);
+
+  const commanderPath    = path.join(purgedModules, 'commander');
+  const commanderContent = fs.readdirSync(commanderPath);
+
+  assert.deepStrictEqual(commanderContent, ['LICENSE', 'index.js', 'package.json']);
+
+  const msPath    = path.join(purgedModules, 'ms');
+  const msContent = fs.readdirSync(msPath);
+
+  assert.deepStrictEqual(msContent, ['index.js', 'license.md', 'package.json']);
 }).catch(console.error);
 
 
